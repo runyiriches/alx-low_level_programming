5927ed51800a6a3d76de388cf60c084af4b7fbe5
runyiriches <runyiriches120@gmail.com>
2023-08-10 16:43:28 -0700

    my commit on 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include stdio.h
+/**
+ * main: A progamme that prints a line using the printf function
+ * Return: always 0 (success)
+ */
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}
