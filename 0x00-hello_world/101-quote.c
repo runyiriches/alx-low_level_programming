63f2154012d1b215f4e083f6096618f7442a6fca
runyiriches <runyiriches120@gmail.com>
2023-08-10 18:53:44 -0700

    commit on 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<unistd.h>
+/**
+* * main - The programe that prints a pices of art os useful
+*Return: Alwaays 1 (success)
+*/
+int main(void)
+{
+	write (2, and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
