bdce8dc7de8b514ba189e8fab0734d5fb29aa59c
runyiriches <runyiriches120@gmail.com>
2023-08-10 19:30:46 -0700

    my commit on 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 #include<unistd.h>
 /**
- ** main - The programe that prints a pices of art os useful
- *Return: Alwaays 1 (success)
+ * main - A programme that print a line to the standard error
+ * Return: 1 (success)
  */
-int main(void);
+int main(void)
 {
-	fput("and that piece of art is useful\"  - Dora Korpar, 2015-10-19\n", stdout);
+	fput("and that piece of art is useful - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
