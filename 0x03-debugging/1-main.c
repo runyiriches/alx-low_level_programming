ce24c28559bb69e994ed0091e98c425b0317b41b
runyiriches <runyiriches120@gmail.com>
2023-08-17 06:49:14 -0700

    commit 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
-
 /**
- * * main - causes an infinite loop
- * * Return: 0
- * */
-
-int main(void) 
+ * main - causes an infinite loop
+ * Return: 0
+ */
+int main(void)
 {
 	int i;
 
 	printf("infinite loop incoming :(\n");
-	
+
 	i = 0;
 
 	/*while (i < 10)*/
@@ -19,6 +17,5 @@ int main(void)
 	/*)*/
 
 	printf("infinte loop avoided \\o/\n");
-	
 	return (0);
 }
