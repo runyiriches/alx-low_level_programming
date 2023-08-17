c73744aaf56ca7c6efa52753c8d888b31ad65960
runyiriches <runyiriches120@gmail.com>
2023-08-17 09:55:39 -0700

    commit to 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -8,14 +8,14 @@
  * Return: largest number
  */
 int largest_number(int a, int b, int c)
-
 {
-int largest;
+	int largest;
+
 	if (a >= b && a >= c)
 	{
 		largest = a;
 	}
-	else if (b >= a && b >=  c)
+	else if (b >= a && b >= c)
 	{
 		largest = b;
 	}
@@ -23,5 +23,5 @@ int largest;
 	{
 		largest = c;
 	}
-return (largest);
+	return (largest);
 }
