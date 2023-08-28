47d41bba3136d1127ace0fb7e3c51678e6f4b680
runyiriches <runyiriches120@gmail.com>
2023-08-28 15:41:40 -0700

    0x00. C - Hello, World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,15 @@
-#include <stdio.h>
 #include <unistd.h>
+#include <stdio.h>
+
 /**
- * main - A programme that print a line to the standard error
- * Return: Always 1 (success)
+ * main - Entry point of the program
+ *
+ * Return: Always 1 (Error)
  */
 int main(void)
 {
 	write(2,
-		"and that piece of art is useful\ - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
+	return (1); /* Return 1 to indicate an error */
 }
