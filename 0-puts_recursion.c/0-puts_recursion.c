75f7500e54c09a3fd08c8a7fc52077735b9b25fb
runyiriches <runyiriches120@gmail.com>
2023-08-30 12:57:42 -0700

    0-puts_recursion.c

--- 0-puts_recursion.c/0-puts_recursion.c
+++ 0-puts_recursion.c/0-puts_recursion.c
@@ -4,11 +4,11 @@
  * @s: input
  * Return: Always 0 (success)
  */
- void _puts_recursion(char *s)
+void _puts_recursion(char *s)
 {
-	if(*s)
+	if (*s)
 	{
-		_putchar(s);
+		_putchar(s)e
 		_puts_recursion(s + 1);
 	}
 	else
